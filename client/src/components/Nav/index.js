b0a5b5bee414660bb699ffa64b1fb54bef217043
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2020-02-17 22:48:01 -0600

    nav refactoring, SVG attempt

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,14 +2,38 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="bg-blue-100 fixed-top">
+    <>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          <span className="font-semibold text-xl tracking-light">Google Books Search</span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Search
+            </a>
+            <a href="/books/saved" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Saved Books
+            </a>
+          </div>
+          Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        </div>
+      </nav>
+      {/* <nav className="bg-blue-100 fixed-top">
       <a className="navbar-brand" href="/">
         Search
       </a>
       <a className="navbar-brand" href="/books/saved">
         Saved Books
       </a>
-    </nav>
+    </nav> */}
+    </>
   );
 }
 
